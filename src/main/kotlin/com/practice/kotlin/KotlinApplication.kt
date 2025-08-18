b6089fe97d8f741baab6a09aa8f34d75295079da
KATE (Eun Ji Sim) <46472768+KATEKEITH@users.noreply.github.com>
2025-08-18 15:24:24 +0900

    chore: add note about program entry point in Kotlin

--- src/main/kotlin/com/practice/kotlin/KotlinApplication.kt
+++ src/main/kotlin/com/practice/kotlin/KotlinApplication.kt
@@ -6,6 +6,9 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class KotlinApplication
 
+// # 프로그램 진입점 ⭐️⭐️⭐️
+// Java 에서는 public static void main(String[] args) 메서드를 사용하여 프로그램을 실행
+// Kotlin 에서는 fun main() 함수를 사용하여 프로그램을 실행
 fun main(args: Array<String>) {
 	runApplication<KotlinApplication>(*args)
 }
