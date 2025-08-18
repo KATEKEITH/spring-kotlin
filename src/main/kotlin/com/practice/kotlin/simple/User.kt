b9c6649ad60f81c935c925760fe90aa25d52c90b
KATE (Eun Ji Sim) <46472768+KATEKEITH@users.noreply.github.com>
2025-08-18 16:33:33 +0900

    chore: var, val

--- src/main/kotlin/com/practice/kotlin/simple/User.kt
+++ src/main/kotlin/com/practice/kotlin/simple/User.kt
@@ -1,1 +1,5 @@
+
+// # var, val ⭐️⭐️⭐️
+// Java 에서는 변수를 선언할 때 final 키워드를 사용하여 불변 변수를 선언합니다.
+// Kotlin 에서는 var 키워드를 사용하여 변수를 선언하고, val 키워드를 사용하여 불변 변수를 선언합니다.
 data class User(val name: String, val age: Int)
